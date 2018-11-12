abadedb562a54a028bbad96fcbca31e8ce2f629a
Sergey Zelenov <github@zelenov.su>
2018-11-12 23:15:35 +0100

    Update sum.test.js

--- 0-module/1-task/test/sum.test.js
+++ 0-module/1-task/test/sum.test.js
@@ -5,8 +5,4 @@ describe('function `sum` test suits', () => {
   it('should sum two numbers', () => {
     expect(sum(1, 2)).to.equal(3);
   });
-  
-  it('should throw TypeError if arguments are not numbers', () => {
-    expect(() => sum('1', [])).throw(TypeError);
-  });
 });
